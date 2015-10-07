05b2f60e91d3fe3c8d5c449440a5d55cd97adfeb
Sergey Rubets <srubets@speroteck.com>
2015-10-07 15:28:39 +0300

    add some function to the range.js

--- source/assets/js/range.js
+++ source/assets/js/range.js
@@ -5,7 +5,12 @@
     var range = function() {
 
         var my = {},
-            _sliderCircle = [];
+            _sliderCircle = $('.range__circle'),
+            _slider = _sliderCircle.closest('.range');
+
+        /* ------- Setup listeners  ------- */
+        _sliderCircle.on('mousedown', _downSlider);
+        _sliderCircle.on('mouseup', _upSlider);
 
         publicInterface();
         init();
@@ -15,21 +20,49 @@
         function publicInterface() {
             my = $.extend(my, {
                 getOpacityValue: function(range) {
-                    _sliderCircle = range;
+                    _sliderCircle = range; // TODO it is doesn't change _sliderCircle, why ?
                     _moveSlider();
                 }
             });
         }
 
+
         /* ----- Узнаем ширину ползунка ----- */
         function _getSliderWidth() {
             return _sliderCircle.closest('.range').width();
         }
-        
-        function _moveSlider() {
-            var sliderWidth =_getSliderWidth();
-            console.log(sliderWidth);
+
+
+        /* ------- Перемещение ползунка -------- */
+        function _getOpacity() {
+            var sliderWidth =_getSliderWidth(),
+                circlePosition = _sliderCircle.position().left,
+                min = 0,
+                max = sliderWidth;
+
+            _moveSlider();
         }
+
+        function _downSlider() {
+            _sliderCircle.css({'position':'absolute'});
+        }
+
+        function _upSlider() {
+            console.log('hi');
+            _sliderCircle.css({'position':'static'});
+        }
+
+        function _moveSlider() {
+
+            _slider.on('mousemove', function(e) {
+                _sliderCircle.css({
+                    'left' : e.offsetX + 'px'
+                });
+                console.log('move');
+            });
+
+        }
+
 
         return my;
     };
@@ -39,4 +72,4 @@
 
 })(window, jQuery);
 
-range.getOpacityValue($('.range'));
+range.getOpacityValue($('.range__circle'));
